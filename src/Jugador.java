9f48a640d3c14346fe879d0b58dd1df150355149
RichardPadilla7 <richardpadilla3rod@gmail.com>
2024-11-15 06:46:18 -0500

    Richard Padilla

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,15 +1,89 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Jugador {
+    protected String equipo; //protected lo que hace es proteger los atributos de la clase padre (Jugador)
+    protected int edad;
+    protected String nombre;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    //CONSTRUCTOR PADRE
+    public Jugador(String equipo, String nombre, int edad) {
+        this.equipo = equipo;
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+
+    // METODO PADRE
+    public void biografia() {
+        System.out.println("Nombre del equipo: " + equipo);
+        System.out.println("Nombre del jugador: " + nombre);
+        System.out.println("Edad del jugador: " + edad);
+    }
+
+    static class Portero extends Jugador {
+        private int atajadas;
+        private int golesRecibidos;
+
+        public Portero(String equipo, String nombrePortero, int edadPortero, int atajadas, int golesRecibidos) {
+            super(equipo, nombrePortero, edadPortero);
+            this.atajadas = atajadas;
+            this.golesRecibidos = golesRecibidos;
+        }
+
+        public int Atajadas(int atajadas) {
+            return atajadas;
+        }
+
+        public int GolesRecibidos(int golesRecibidos) {
+            return golesRecibidos;
+        }
+
+        @Override //Sobreescribir
+        public void biografia() {
+            super.biografia();
+            System.out.println("Atajadas: " + Atajadas(atajadas));
+            System.out.println("Goles recibidos: " + GolesRecibidos(golesRecibidos) + "\n");
         }
     }
+
+
+
+    static class Defensa extends Jugador {
+        private int bloqueos;
+
+        //Aqui tambien le cambie los nombres para diferenciar
+        public Defensa(String equipo, String nombreDefensa, int edadDefensa, int bloqueos) {
+            super(equipo, nombreDefensa, edadDefensa);
+            this.bloqueos = bloqueos;
+        }
+
+        public int Bloqueos(int bloqueos) {
+            return bloqueos;
+        }
+
+        @Override // sobreescribimos
+        public void biografia() {
+            super.biografia();
+            System.out.println("Bloqueos: " + Bloqueos(bloqueos) + "\n");
+        }
+    }
+
+
+
+    static class Delantero extends Jugador {
+        public Delantero(String equipo, String nombreDelantero, int edadDelantero){
+            super(equipo, nombreDelantero, edadDelantero);
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        Portero portero = new Portero("Liga","Richard",20,15,20);
+        portero.biografia();
+
+        Defensa defensa = new Defensa("Liga","Patricio",22,50);
+        defensa.biografia();
+
+        Delantero delantero = new Delantero("Liga","Ariel",25);
+        delantero.biografia();
+    }
 }
+
